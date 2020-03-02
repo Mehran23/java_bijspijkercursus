7f7eeb00a5ec03919dec4ce01b88c0ff5af52ec6
Mehran <mshahbackhsh@roc-dev.com>
2020-03-02 08:50:45 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,71 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    	//OPDRACHT 1.1
+		System.out.println("\nOpdracht 1.1\n");
+	    System.out.println("Java is Object Georiënteerd");
+	    System.out.println("Java is strongly typed");
+	    System.out.println("Java is geen JavaScript");
+
+	    //OPDRACHT 1.2
+		System.out.println("Opdracht1.2\n");
+		System.out.println("Java is geweldig!");
+		System.out.println("Java is geweldig!");
+		System.out.println("Java is geweldig!");
+		System.out.println("Java is geweldig!");
+		System.out.println("Java is geweldig!\n\n");
+
+		//OPDRACHT 1.3
+		System.out.println("Opdracht 1.3\n");
+		System.out.println("a\ta^2\ta^3\ta^4");
+		System.out.println("1\t1\t1\t1");
+		System.out.println("2\t4\t8\t16");
+		System.out.println("3\t9\t27\t81");
+		System.out.println("4\t16\t64\t256\n\n");
+
+		//OPDRACHT 1.4
+		System.out.println("Opdracht 1.4\n");
+		double teller= 7.5 * 6.5 - 4.5 * 3;
+		double noemer= 47.5 * 5.5;
+		double breuk= teller / noemer;
+		System.out.println(breuk + "\n");
+
+		double optie2= (7.5 * 6.5 - 4.5 * 3) / (47.5 * 5.5);
+		System.out.println(optie2 + "\n\n");
+
+		//OPDRACHT 1.5
+		System.out.println("Opdracht 1.5\n");
+		int optelsom= (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+		System.out.println(optelsom + "\n\n");
+
+		//OPDRACHT 1.6
+		System.out.println("Opdracht 1.6\n");
+		double benaderingVanPI= 4 * (1 - 1.0  / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11 + 1.0 / 13);
+		System.out.println(benaderingVanPI + "\n\n");
+
+		//OPDRACHT 1.7
+		System.out.println("Opdracht 1.7\n");
+		double TT= 3.14159;
+		double straal= 6.5;
+		double oppervlakte= straal * straal * TT;
+		double omtrek= 2 * straal * TT;
+		System.out.println("oppervlakte=" + oppervlakte);
+		System.out.println("omtrek=" + omtrek + "\n");
+
+		//OPDRACHT 1.8
+		System.out.println("Opdracht 1.8\n");
+		double lengte= 5.3;
+		double breedte= 8.6;
+		double Oppervlakte= lengte * breedte;
+		double Omtrek= 2 * (lengte + breedte);
+		System.out.println("oppervlakte=" + Oppervlakte);
+		System.out.println("omtrek=" + Omtrek);
+
+
+
+
+	}
+}
